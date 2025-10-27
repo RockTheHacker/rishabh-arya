1d9066ee74f0beaf3c56a28162c3bad5ad8a0f8c
R.K Music <rk8006969806@gmail.com>
2025-10-27 10:10:53 +0000

    Use pp.png in public folder as fevicon that too in rounded form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        <link rel="icon" href="/pp.png" sizes="any" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><defs><clipPath id=%22clip%22><circle cx=%2250%22 cy=%2250%22 r=%2250%22/></clipPath></defs><g clip-path=%22url(%23clip)%22><image href=%22/pp.png%22 width=%22100%22 height=%22100%22/></g></svg>"/>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Space+Grotesk:wght@500;700&family=Source+Code+Pro:wght@400;500&display=swap" rel="stylesheet" />
